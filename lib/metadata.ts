bb663f9c071a1924cd1359be29e241572d89e330
deeppulserecords <deeppulselabel@gmail.com>
2026-06-25 17:51:43 +0100

    Delete lib directory

--- lib/metadata.ts
+++
@@ -1,8 +0,0 @@
-export const siteConfig = {
-  name: "DEEPPULSE RECORDS",
-  description:
-    "DeepPulse is a wellness music label and creative ecosystem dedicated to ambient, meditative and healing soundscapes.",
-  url: "https://deeppulserecords.com",
-  instagramUrl: "https://www.instagram.com/deeppulserecords/",
-  email: "deeppulselabel@gmail.com"
-};
